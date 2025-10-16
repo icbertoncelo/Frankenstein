04b78b6d96e98fa52cf58ad5317c291868b1ac3e
icbertoncelo <carlos.ian007@gmail.com>
2025-10-16 20:54:44 -0300

    fix: remove vite nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
